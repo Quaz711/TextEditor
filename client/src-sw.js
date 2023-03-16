baa4f2691d764700d626ed97f619af5cdf70b1fc
Anthony <quaz711@hotmail.com>
2023-03-16 05:05:58 -0400

    more bugs edited for heroku

--- client/src-sw.js
+++ client/src-sw.js
@@ -37,6 +37,11 @@ registerRoute(
       plugins: [
         new CacheableResponsePlugin({
           statuses: [0, 200]
+        }),
+
+        new ExpirationPlugin({
+          maxEntries: 60,
+          maxAgeSeconds: 30 * 24 * 60 * 60
         })
       ]
     })
